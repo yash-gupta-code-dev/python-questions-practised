dec0a086435090594955604f284e4b7f344cbb3c
Yash Gupta <yashgupta.epicinfocus@gmail.com>
2026-07-23 17:33:37 +0530

    Added 2 new questions and answer

--- Employee Count by Department.py
+++ Employee Count by Department.py
@@ -6,3 +6,21 @@ employees = [
     ("Eva", "Finance"),
     ("John", "IT")
 ]
+
+
+def count_employees(employees):
+    employee_dict = {}
+    for employee in employees:
+        if employee[1] not in employee_dict:
+            employee_dict[employee[1]] = {
+                "employees" : [employee[0]],
+                "count" : 1
+            }
+        else:
+            employee_dict[employee[1]]["count"] += 1
+            employee_dict[employee[1]]["employees"].append(employee[0])
+
+    return employee_dict
+
+employee_dict = count_employees(employees)
+print(employee_dict)
